d4e355a59d3cb6074d5181041c6f31e382331013
Nallely House <nallelyht@gmail.com>
2017-04-10 09:53:02 -0500

    no subi el codigo

--- 17-programacion-funcional/ejercicio-01/ejercicio-01.js
+++ 17-programacion-funcional/ejercicio-01/ejercicio-01.js
@@ -1,6 +1,37 @@
 function isValidCard(creditCardNumber) {
-    // Escribe tu código aquí
     
+    // Escribe tu código aquí
+    if(creditCardNumber == null || creditCardNumber == undefined) { //Prueba 1
+      return "Ingresa un numero";
+  }    if(typeof creditCardNumber != "number"){ //Prueba 2
+      return "Error de dato";
+  }    if(dieciseisNumeros(creditCardNumber) != true){
+      return "Faltan numeros";
+  }
+ 
+function dieciseisNumeros(creditCardNumber) {
+   numero = creditCardNumber.toString;
+   if(numero.length == 16){
+       return true;
+   }
+}
+    var longitud = creditCardNumber.length;
+    var par = longitud % 2;
+    var sum = 0;
+    for (var i = longitud-1; i >= 0; i--) {
+        var num = parseInt(creditCardNumber.charAt(i));
+        if (i % 2 == par) { num *= 2; }
+        if (num > 9) { num -= 9; }
+        sum += num;
+    }
+    var result = sum%10;
+    
+    if ( result == 0){
+        return true;
+    } else{
+        return false;
+    }
+
 }
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
